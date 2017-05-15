2b028512192935e26763c5acfe7026dfbbd13369
Ryan H <Ryan H>
2017-05-14 18:42:17 -0700

    The newer version of what we did today with right motor not commented out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode1.java
@@ -1,8 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
 /**
- * Created by Dan on 5/14/2017.
+ * Created by Ryan on 5/14/2017.
  */
 
-public class Opmode1 {
+@TeleOp(name="Opmode1", group="Ryan")  // @Autonomous(...) is the other common choice
+
+public class Opmode1 extends OpMode {
+
+    HardwarePushbot robot = new HardwarePushbot();
+    DcMotor leftMotor = null;
+    DcMotor rightMotor = null;
+
+    int count;
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+
+
+        leftMotor = hardwareMap.dcMotor.get("left_drive");
+        rightMotor = hardwareMap.dcMotor.get("right_drive");
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+
+    @Override
+    public void loop() {
+        if (gamepad1.a == true) {
+
+            robot.leftMotor.setPower(0.5);
+            robot.rightMotor.setPower(0);
+        }else  {
+            robot.leftMotor.setPower(0);
+            robot.rightMotor.setPower(0);
+        }
+        if (gamepad1.b == true) {
+
+            robot.rightMotor.setPower(0.5);
+            robot.leftMotor.setPower(0);
+
+        }else {
+            robot.leftMotor.setPower(0);
+            robot.rightMotor.setPower(0);
+        }
+    }
 }
