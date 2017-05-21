7174ee20a9c24976759bb14826f4fceb772b4905
aidan <aidan>
2017-05-21 10:08:17 -0700

    rc car final

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RcOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RcOpMode.java
@@ -15,19 +15,24 @@ import static java.lang.Thread.sleep;
 public class RcOpMode extends OpMode {
 
     HardwarePushbot robot = new HardwarePushbot();
+    float left;
+    float right;
 
     @Override
     public void init() {
         robot.init(hardwareMap);
-
     }
 
     @Override
     public void loop() {
-double max = Math.max(Math.abs(-gamepad1.left_stick_y - gamepad1.right_stick_x), Math.abs(-gamepad1.left_stick_y + gamepad1.right_stick_x));
-        robot.leftMotor.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x);
-        robot.rightMotor.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
-
-
+        left = -gamepad1.left_stick_y - gamepad1.right_stick_x;
+        right = -gamepad1.left_stick_y + gamepad1.right_stick_x;
+        double max = Math.max(Math.abs(left), Math.abs(right));
+        if (max > 1) {
+            left /= max;
+            right /= max;
+        }
+        robot.leftMotor.setPower(left);
+        robot.rightMotor.setPower(right);
     }
 }
