a5e2bab2042528e98debae7e81bfd7183a957800
aidan <aidan@robogo.com>
2017-06-24 21:51:24 -0700

    allows user to get ground, tape, and perfect value before running an opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConfigureGroundAndTapeValues.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.internal.AppUtil;
+
+/**
+ * Created by xinchen on 6/24/2017.
+ */
+@Autonomous(name = "set ground and tape values", group = "Aidan")
+public class ConfigureGroundAndTapeValues extends LinearOpMode {
+    HardwarePushbot robot = new HardwarePushbot();
+    OpticalDistanceSensor ods;
+    TouchSensor touch;
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        ods = hardwareMap.opticalDistanceSensor.get("sensor_ods"); // names of the stuff we need to set in the config.
+        ods.enableLed(true);
+        touch = hardwareMap.touchSensor.get("sensor_touch");
+        double ground = ods.getLightDetected();
+        while (!touch.isPressed()) {
+            this.sleep(100);
+        }
+        double tape = ods.getLightDetected();
+        while (!touch.isPressed()) {
+            this.sleep(100);
+        }
+        double perfectValue = ods.getLightDetected();
+        String values = String.format("%f,%f,%f", ground, tape, perfectValue);
+        ReadWriteFile.writeFile(AppUtil.FIRST_FOLDER, "ods.data", values);
+    }
+}
