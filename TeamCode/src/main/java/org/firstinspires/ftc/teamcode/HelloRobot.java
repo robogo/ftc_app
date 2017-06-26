707c3f9d86ae7ceba00af46a7d80e4c329cc2cd0
xinchen <xinchen@robogo>
2017-06-26 11:03:38 -0700

    testing new methods of EnvLight in a class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloRobot.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 //import com.robogo.Mock;
@@ -20,13 +18,14 @@ public class HelloRobot extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
+        EnvLight stuff = new EnvLight();
         lineSensor = hardwareMap.opticalDistanceSensor.get("sensor_ods");
         lineSensor.enableLed(true);
         robot.rightClaw.setPosition(Servo.MIN_POSITION);
         //((Mock)lineSensor).setData(new double[]{5, 5.5}, new double[]{0.5, 0});
         double[] values = null;
         try {
-            values = OdsValueReader.read();
+            values = EnvLight.read();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,28 +43,55 @@ public class HelloRobot extends LinearOpMode {
             // if not over line, go forward with 1.0
             robot.leftMotor.setPower(1);
             robot.rightMotor.setPower(1);
-            if (lightLevel > tape) {
-                seenTape = true;
-                robot.leftMotor.setPower(0);
-                robot.rightMotor.setPower(0);
-                telemetry.addData("seen tape", 1);
-                sleep(1000);
-                if (lineSensor.getLightDetected() > tape) {
+            try {
+                if (lightLevel > tape && stuff.isTapeColorWhite()) {
+                    seenTape = true;
+                    robot.leftMotor.setPower(0);
+                    robot.rightMotor.setPower(0);
+                    telemetry.addData("seen tape", 1);
+                    sleep(1000);
+                    if (lineSensor.getLightDetected() > tape) {
+                        positionedCorrectly = true;
+                        telemetry.addData("arrived", 1);
+                    }
+                }else if (lightLevel < tape && stuff.isTapeColorWhite() == false) {
+                    seenTape = true;
+                    robot.leftMotor.setPower(0);
+                    robot.rightMotor.setPower(0);
+                    telemetry.addData("seen tape", 1);
+                    sleep(1000);
+                    if (lineSensor.getLightDetected() > tape) {
+                        positionedCorrectly = true;
+                        telemetry.addData("arrived", 1);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // if over, go backward with 0.1
+            try {
+                if (seenTape == true && lineSensor.getLightDetected() < tape && stuff.isTapeColorWhite()) {
+                    while (lineSensor.getLightDetected() < tape) {
+                        robot.rightMotor.setPower(-0.1);
+                        robot.leftMotor.setPower(-0.1);
+                        telemetry.addData("back", 1);
+                    }
                     positionedCorrectly = true;
                     telemetry.addData("arrived", 1);
                 }
-            }
-            // if over, go backward with 0.1
-            if (seenTape == true && lineSensor.getLightDetected() < tape) {
-                while (lineSensor.getLightDetected() < tape) {
-                    robot.rightMotor.setPower(-0.1);
-                    robot.leftMotor.setPower(-0.1);
-                    telemetry.addData("back", 1);
+                if (seenTape == true && lineSensor.getLightDetected() > tape && stuff.isTapeColorWhite() == false) {
+                    while (lineSensor.getLightDetected() < tape) {
+                        robot.rightMotor.setPower(-0.1);
+                        robot.leftMotor.setPower(-0.1);
+                        telemetry.addData("back", 1);
+                    }
+                    positionedCorrectly = true;
+                    telemetry.addData("arrived", 1);
                 }
-                positionedCorrectly = true;
-                telemetry.addData("arrived", 1);
-            }
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             telemetry.update();
 
             // stop on the line
